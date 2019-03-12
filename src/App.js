fda47849a548b8666416a1ba3a50b5e843e1fa05
zhouxue <zhoux0130@hotmail.com>
2019-03-12 18:02:08 +0800

    完成第二课的练习

--- src/App.js
+++ src/App.js
@@ -1,31 +1,60 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
+import React, {Component} from 'react';
 import 'bootstrap/dist/css/bootstrap.css'
 import './App.scss';
 
 class App extends Component {
-  constructor(props){
-    super(props);
-    this.state = {
-      item : [
-          {id:1, name: 'deja vu'},
-          {id:2, name: 'deja vu1'},
-          {id:3, name: 'deja vu2'}
-      ]
+    constructor(props) {
+        super(props);
+        this.state = {
+            items: [
+                {id: 1, name: 'deja vu'},
+                {id: 2, name: 'Cras justo odio'},
+                {id: 3, name: 'deja vu2'}
+            ]
+        }
     }
-  }
 
-  render() {
-    return (
-        <ul className="list-group">
-          <li className="list-group-item">Cras justo odio</li>
-          <li className="list-group-item">Dapibus ac facilisis in</li>
-          <li className="list-group-item">Morbi leo risus</li>
-          <li className="list-group-item">Porta ac consectetur ac</li>
-          <li className="list-group-item">Vestibulum at eros</li>
-        </ul>
-    );
-  }
+    move(index, diff) {
+        var items = this.state.items;
+        var item = items[index];
+        items.splice(index, 1);
+        items.splice(index + diff, 0, item);
+        this.setState({items: items});
+    }
+
+    moveUp(index) {
+        this.move(index, -1);
+    }
+
+    moveDown(index) {
+        this.move(index, 1);
+    }
+
+    render() {
+        return (
+            <div className="items-panel">
+                <ul className="list-group">
+                    {
+                        this.state.items.map((item, index) => (
+                            <li className="list-group-item">
+                                <div className="name">{item.name}</div>
+                                <div className="btn-group">
+                                    {
+                                        index > 0 ?
+                                            <button className="btn btn-light" onClick={()=>this.moveUp(index)}>UP</button> : null
+                                    }
+                                    {
+                                        index < this.state.items.length -1  ?
+                                            <button className="btn btn-light" onClick={()=>this.moveDown(index)}>DOWN</button> : null
+                                    }
+                                </div>
+                            </li>
+                        ))
+                    }
+                </ul>
+            </div>
+        );
+    }
 }
 
 export default App;
