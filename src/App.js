3e34fc1f2117f9a17a073b3c8207aa3cf0335a80
zhouxue <zhoux0130@hotmail.com>
2019-03-12 17:44:09 +0800

    add

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import 'bootstrap/dist/css/bootstrap.css'
+import './App.scss';
 
 class App extends Component {
+  constructor(props){
+    super(props);
+    this.state = {
+      item : [
+          {id:1, name: 'deja vu'},
+          {id:2, name: 'deja vu1'},
+          {id:3, name: 'deja vu2'}
+      ]
+    }
+  }
+
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
-      </div>
+        <ul className="list-group">
+          <li className="list-group-item">Cras justo odio</li>
+          <li className="list-group-item">Dapibus ac facilisis in</li>
+          <li className="list-group-item">Morbi leo risus</li>
+          <li className="list-group-item">Porta ac consectetur ac</li>
+          <li className="list-group-item">Vestibulum at eros</li>
+        </ul>
     );
   }
 }
